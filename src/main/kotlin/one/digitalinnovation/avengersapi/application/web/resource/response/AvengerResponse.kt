38da12d34dc154bf7fc1e8d03e18d3ad4473ef5a
Fabio Kusaba <fabinhokb44@gmail.com>
2024-05-17 18:39:45 -0300

    added static factory method

--- src/main/kotlin/one/digitalinnovation/avengersapi/application/web/resource/response/AvengerResponse.kt
+++ src/main/kotlin/one/digitalinnovation/avengersapi/application/web/resource/response/AvengerResponse.kt
@@ -1,8 +1,19 @@
 package one.digitalinnovation.avengersapi.application.web.resource.response
 
+import one.digitalinnovation.avengersapi.domain.avenger.Avenger
+
 data class AvengerResponse(
     val nick: String,
     val person: String,
-    val description: String,
-    val history: String
-)
+    val description: String?,
+    val history: String?
+) {
+    companion object {
+        fun from(avenger: Avenger) = AvengerResponse(
+            nick = avenger.nick,
+            person = avenger.person,
+            description = avenger.description,
+            history = avenger.history
+        )
+    }
+}
