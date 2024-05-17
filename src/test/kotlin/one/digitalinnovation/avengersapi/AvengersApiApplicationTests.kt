0619df8f15e1ce8833d0a90112e38533ea303b08
Fabio Kusaba <fabinhokb44@gmail.com>
2024-05-17 07:40:38 -0300

    created avengers api first version

---
+++ src/test/kotlin/one/digitalinnovation/avengersapi/AvengersApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package one.digitalinnovation.avengersapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AvengersApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
