e395cda6b22610edc583fc5e1c212b8db3d88e69
Fabio Kusaba <fabinhokb44@gmail.com>
2024-05-17 12:03:43 -0300

    added request validation

--- src/main/kotlin/one/digitalinnovation/avengersapi/application/web/request/AvengerRequest.kt
+++ src/main/kotlin/one/digitalinnovation/avengersapi/application/web/request/AvengerRequest.kt
@@ -1,8 +1,21 @@
 package one.digitalinnovation.avengersapi.application.web.request
 
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.NotEmpty
+import jakarta.validation.constraints.NotNull
+
 data class AvengerRequest(
+    @field:NotNull
+    @field:NotBlank
+    @field:NotEmpty
     val nick: String,
+
+    @field:NotNull
+    @field:NotBlank
+    @field:NotEmpty
     val person: String,
-    val description: String,
-    val history: String
+
+    val description: String? = "",
+
+    val history: String? = ""
 )
